2a0e1c69509c9453a1c420e1d392cac293aedc2a
Steven Lucas (VDAB) <steven.lucas@vdab.be>
2021-02-17 20:09:31 +0100

    Delete FLYNET directory

--- FLYNET/Afdeling.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FLYNET.Personeel
-{
-    public enum Afdeling { Personeelsdienst, Boekhouding, Incheckbalie, Logistiek }
-    
-}
